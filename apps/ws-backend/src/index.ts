ce4d6d9ff53fd5af6c88b7004d044973bf48a619
Mruthunjay P N <muttu@mpn.dev>
2025-02-15 09:41:21 +0530

    added simple ws layer chat-logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,31 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 import { JWT_SECRET } from "@repo/backend-common/config";
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  ws : WebSocket
+  rooms : String[],
+  userId : String
+}
+
+const users :User[] = [];
+
+function checkUser(token : string): string | null{
+  const decoded = jwt.verify(token , JWT_SECRET)
+  //the typescript may complain about the decoded - whether stirng or payload ,so
+  if(typeof decoded == "string"){
+    return null ;
+  }
+  if(!decoded || !decoded.id ){
+  //other way to check decoded is string or jwtpayload - if above typeof check is not performed  
+  // if(!decoded || !(decoded as JwtPayload).userId){
+    return null;
+  }
+  return decoded.id;
+}
+
 wss.on('connection', function connection(ws , request) {
-  
   //extracting token from the query , and allowing user only if he is authorized by the http backend
   //we will use the same token  of http to verify the user  
   const url = request.url ; 
@@ -13,22 +34,47 @@ wss.on('connection', function connection(ws , request) {
   }
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || ""; 
-
-  const decoded = jwt.verify(token , JWT_SECRET)
-  //the typescript may complain about the decoded - whether stirng or payload ,so
-  if(typeof decoded == "string"){
-    ws.close();
-    return;
-  }
-  if(!decoded || !decoded.userId ){
-
-  //other way to check decoded is string or jwtpayload - if above typeof check is not performed  
-  // if(!decoded || !(decoded as JwtPayload).userId){
+  const userId = checkUser(token);
+  if(!userId){
     ws.close();
     return
   }
 
+  users.push({
+    userId ,
+    rooms : [],
+    ws
+  })
   ws.on('message', function message(data) {
-    ws.send('something');
+    const parsedData =JSON.parse(data as unknown as string); 
+
+    if(parsedData.type=== "join_room"){
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parsedData.roomID) ;    //checks -> does this room exist? acces control , 
+    }
+
+    if(parsedData.type === "leave_room"){
+      const user = users.find(x => x.ws === ws);
+      if(!user){
+        return
+      }
+      user.rooms = user.rooms.filter(x => x === parsedData.roomId) 
+      //removing a user from a specific room
+    }
+
+    if(parsedData.type === "chat"){
+      const roomId = parsedData.roomID;
+      const message = parsedData.messgae ;  //checks : message is not too long, doenst have anythng vulgar etcc.
+
+      users.forEach(user => {
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type : "chat" , 
+            message : message , 
+            roomId
+          }));
+        }
+      })
+    }
   });  
 });
